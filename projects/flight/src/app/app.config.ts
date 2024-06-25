2c863233c6b4311d329fe4400e7e8947099fea5b
mikezks <23002661+mikezks@users.noreply.github.com>
2024-06-25 12:02:55 +0200

    feat: hydrate rx form state by using inject, router component input, rxjs interop, effect

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -1,14 +1,13 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideEffects } from '@ngrx/effects';
-import { provideStore } from '@ngrx/store';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
-import { provideRouterFeature } from './shared/logic-router-state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(APP_ROUTES),
+    provideRouter(APP_ROUTES,
+      withComponentInputBinding()
+    ),
     provideHttpClient()
   ]
 };
--- projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,7 +1,11 @@
 import { NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, Signal, effect, inject, input, numberAttribute, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
+import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
+import { Passenger, initialPassenger } from '../../logic-passenger';
+import { switchMap } from 'rxjs';
 
 
 @Component({
@@ -14,6 +18,22 @@ import { validatePassengerStatus } from '../../util-validation';
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
+  // Inject method to access Dependency Injection
+  private passengerService = inject(PassengerService);
+
+  // Reactive Signal Input -> Router provides ID from URL
+  id = input.required<number, string>({
+    transform: numberAttribute
+  });
+
+  // Interop between Signals and RxJS
+  // Using Operator to fetch State from Backend API
+  private passenger = toSignal(
+    toObservable(this.id).pipe(
+      switchMap(id => this.passengerService.findById(id))
+    ), { initialValue: initialPassenger }
+  );
+
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
     firstName: [''],
@@ -23,6 +43,11 @@ export class PassengerEditComponent {
       validatePassengerStatus(['A', 'B', 'C'])
     ]]
   });
+
+  constructor() {
+    // We react on Signal changes to update the Reactive Forms value.
+    effect(() => this.editForm.patchValue(this.passenger()));
+  }
 
   protected save(): void {
     console.log(this.editForm.value);
