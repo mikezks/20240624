3a8042066bb6c4b49b45b9cfd6e7cbf845309335
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-22 09:57:48 +0100

    feat: generic navigation feature library

---
+++ projects/navigation-lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of navigation-lib
+ */
+
+export { provideNavigationService, provideNavigationConfig } from './lib/logic-navigation'
+export { NavigationComponent } from './lib/ui-common'
+export { NavigationConfig } from './lib/model-navigation'
